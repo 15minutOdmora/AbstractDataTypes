d221689106d86bbccb450dd9c11ea558f9793cb4
15minutOdmora <liammislej@gmail.com>
2021-05-14 11:31:26 +0200

    Created basic files and Stack class

---
+++ ADT/stack.py
@@ -0,0 +1,76 @@
+
+class Stack:
+    def __init__(self):
+        self._data = []
+        self._counter = 0
+
+    def empty(self) -> bool:
+        """
+        Function checks if stack is empty
+        :return: True/False
+        """
+        return self._counter == 0
+
+    def push(self, element) -> None:
+        """
+        Method adds element to the stack
+        """
+        self._data.append(element)
+        self._counter += 1
+
+    def pop(self) -> None:
+        """
+        Method remove the top element from the stack.
+        :return: None
+        """
+        if self.empty():
+            raise ValueError("pop: The stack is already empty.")
+        self._data.pop()
+
+    def peak(self) -> any:
+        """
+        Method returns the top element of the stack. Does not change the stack.
+        :return: element
+        """
+        if self.empty():
+            raise ValueError("peak: The stack is empty.")
+        return self._data[-1]
+
+    def take(self) -> any:
+        """
+        Method returns the top element from the stack while also removing it.
+        :return:
+        """
+        if self.empty():
+            raise ValueError("take: The stack is empty.")
+        el = self._data[-1]
+        self._data.pop()
+        return el
+
+    def __len__(self) -> int:
+        """
+        Length method for stack, returns number of elements.
+        :return: int
+        """
+        return self._counter
+
+    def __iter__(self) -> iter:
+        """
+        Iter method for stack, creates an iterable from items.
+        :return: list
+        """
+        return iter(self._data)
+
+    def __str__(self) -> str:
+        """
+        String representation of stack object
+        :return:
+        """
+        bt = "Bottom"
+        for elt in self._data:
+            bt += " : " + str(elt)
+        return bt + " : Top"
+
+
+if __name__ == "__main__":
+    pass
