93367a906a36c87326a9194eea50e62d7d9a833e
15minutOdmora <liammislej@gmail.com>
2021-05-14 16:17:38 +0200

    Added some basic functions to binary_tree.py

--- ADT/binary_tree.py
+++ ADT/binary_tree.py
@@ -15,7 +15,12 @@ class BinaryTree:
             self.empty = True
         assert not kwargs  # Can't accept other arguments
 
-    def __repr__(self, shift=''):
+    def __repr__(self, shift='') -> str:
+        """
+        Representation fucntion for binary tree
+        :param shift: str
+        :return: str
+        """
         if self.empty:
             return "BinaryTree()".format(shift)
         elif self.left.empty and self.right.empty:
@@ -28,7 +33,12 @@ class BinaryTree:
                        self.right.__repr__(shift + '              ')
                        )
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
+        """
+        Equality method for two binary trees.
+        :param other: BinaryTree
+        :return: bool
+        """
         if self.empty and other.empty:
             return True
         elif not self.empty and not other.empty:
@@ -37,8 +47,127 @@ class BinaryTree:
         else:
             return False
 
-    def __hash__(self):
+    def __hash__(self) -> hash:
+        """
+        Hash method for binary tree
+        :return: hash
+        """
         if self.empty:
             return hash(())
         else:
             return hash((self.data, self.left, self.right))
+
+    def cut(self, level) -> None:
+        """
+        Method removes all the subtrees from given level.
+        :param level: int
+        :return: None
+        """
+        if level == 0:
+            self.empty = True
+            self.left = None
+            self.right = None
+            self.data = None
+        elif self.empty:
+            pass
+        else:
+            self.left.cut(level-1)
+            self.right.cut(level-1)
+
+    def mirror(self):
+        """
+        Method mirrors all the branches in the tree(and subtrees).
+        :return: None
+        """
+        if not self.empty:
+            self.left, self.right = self.right, self.left
+            self.left.mirror()
+            self.right.mirror()
+
+
+def inorder(tree: BinaryTree) -> list:
+    """
+    Function performs a inorder search of the given tree, returning the list of elements of each node.
+    :param tree: BinaryTree
+    :return: list
+    """
+    if tree.empty:
+        return []
+    data = []
+    data += inorder(tree.left)
+    data += tree.data
+    data += inorder(tree.right)
+    return data
+
+
+def preorder(tree: BinaryTree) -> list:
+    """
+    Function performs a preorder search of the given tree, returning the list of elements of each node.
+    :param tree: BinaryTree
+    :return: list
+    """
+    if tree.empty:
+        return []
+    data = []
+    data += tree.data
+    data += preorder(tree.left)
+    data += preorder(tree.right)
+    return data
+
+
+def postorder(tree: BinaryTree) -> list:
+    """
+    Function performs a postorder search of the given tree, returning the list of elements of each node.
+    :param tree: BinaryTree
+    :return: list
+    """
+    if tree.empty:
+        return []
+    data = []
+    data += postorder(tree.left)
+    data += postorder(tree.right)
+    data += tree.data
+    return data
+
+
+def height(tree: BinaryTree) -> int:
+    """
+    Function calculates the height of the given tree, where height = num. of levels
+    :param tree: BinaryTree
+    :return: int
+    """
+    if tree.empty:
+        return 0
+    else:
+        if tree.left.empty:
+            left_height = 0
+        else:
+            left_height = height(tree.left)
+        if tree.right.empty:
+            right_height = 0
+        else:
+            right_height = height(tree.right)
+        return max(left_height, right_height)
+
+
+def is_leaf(tree: BinaryTree) -> bool:
+    """
+    Function determines if the given binary tree is a leaf(Does not have child's)
+    :param tree: BinaryTree
+    :return: bool
+    """
+    return (not tree.empty) and tree.right.empty and tree.left.empty
+
+
+def number_of_leafs(tree: BinaryTree) -> int:
+    """
+    Function calculates the number of leafs of given tree
+    :param tree: BinaryTree
+    :return: int
+    """
+    if tree.empty:
+        return 0
+    elif is_leaf(tree):
+        return 1
+    return number_of_leafs(tree.left) + number_of_leafs(tree.right)
+
