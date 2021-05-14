a25f974b7ccdf525dcdb0e460037051c52eaed5d
15minutOdmora <liammislej@gmail.com>
2021-05-14 15:11:13 +0200

    Update __init__.py, created binary_tree.py.

---
+++ ADT/__init__.py
@@ -0,0 +1,12 @@
+"""
+Abstract Data Types a Python library containing various abstract data types implemented as classes.
+"""
+
+__title__ = "AbstractDataTypes"
+__author__ = "Liam Mislej"
+__license__ = "MIT License"
+
+from ADT import binary_tree
+from ADT import linked_list
+from ADT import stack
+from ADT import queue
